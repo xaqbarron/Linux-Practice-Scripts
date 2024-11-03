a1f42c735211cc50e417bc1df546c8a8407a5825
xaqbarron <zacbarron89@gmail.com>
2024-11-03 13:31:04 -0600

    Create run3tasks.sh
    Change permissions and execute 3 existing scripts

---
+++ run3tasks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# define list of tasks as array
+scripts=("mathscript.sh" "nameagescript.sh" "salarysortscript2.sh")
+
+# loop through scripts and change permissions to allow all
+for ((i = 0; i < 3; i++)) do
+chmod 777 "${scripts[i]}"
+done
+
+# loop through scripts and execute them
+for ((i = 0; i < 3; i++)) do
+echo "Running ${scripts[i]}"
+./"${scripts[i]}"
+echo "Finished running ${scripts[i]}"
+done
