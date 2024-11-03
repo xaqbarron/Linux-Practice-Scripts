a5d75ee1e21abd1f5354e10473f753c61b1176a8
xaqbarron <zacbarron89@gmail.com>
2024-11-03 13:37:51 -0600

    Update run3tasks.sh

--- run3tasks.sh
+++ run3tasks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # define list of tasks as array
-scripts=("mathscript.sh" "nameagescript.sh" "salarysortscript2.sh")
+scripts=("mathscript.sh" "nameagescript.sh" "salarysortscript.sh")
 
 # loop through scripts and change permissions to allow all
 for ((i = 0; i < 3; i++)) do
