eb7eb3368902e14dda1f718eae0c23a21c533263
xaqbarron <zacbarron89@gmail.com>
2024-11-03 13:28:07 -0600

    Create nameagescript.sh
    Input 3 names and their ages and then print them.

---
+++ nameagescript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#input names and ages
+read -p "Enter 1st name: " name1
+read -p "Enter $name1's age: " age1
+read -p "Enter 2nd name: " name2
+read -p "Enter $name2's age: " age2
+read -p "Enter 3rd name: " name3
+read -p "Enter $name3's age: " age3
+
+#output
+echo "$name1 is $age1 years old."
+echo "$name2 is $age2 years old."
+echo "$name3 is $age3 years old."
